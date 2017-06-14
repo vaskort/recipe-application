45009a0e0acf38fc5b7a047b22d05316830baaa8
Vasilis Kortsimelidis <vaskort@gmail.com>
2017-06-14 20:36:49 +0100

    moved the routes inside Main component
    This will help not mess our ReactDom.render functon inside app.jsx - better to use Main as our main wrapper

--- app/app.jsx
+++ app/app.jsx
@@ -1,26 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom';
 
 import Main from 'Main';
-import RecipeDetail from 'RecipeDetail';
 
 // App css
 require('style-loader!css-loader!foundationStyles');
 require('style-loader!css-loader!sass-loader!appStyles');
 
 ReactDOM.render(
-  <Router>
-    {/*Because both Routes will match we want to stop at the first that's why */}
-    {/*we use Switch - check more at https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/api/Switch.md */}
-    <Switch>
-      <Route path="/recipe/:id" render={()=><RecipeDetail num="2" someProp={100}/>} />
-      <Route path="/" component={Main} />
-    </Switch>
-  </Router>,
+  <Main />,
   document.getElementById('app')
 );
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+import RecipeDetail from 'RecipeDetail';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
 
 import Recipes from 'Recipes';
 
@@ -6,10 +14,14 @@ class Main extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Recipes</h1>
-        <Recipes />
-      </div>
+      <Router>
+        {/* I used the attribute exact in the first route so that not both get matched  */}
+        {/* else we could use the Switch component - check more here: https://github.com/ReactTraining/react-router/blob/master/packages/react-router/docs/api/Switch.md */}
+          <div>
+            <Route exact path="/" component={Recipes} />
+            <Route path="/recipe/:id" render={()=><RecipeDetail num="2" someProp={100}/>} />
+          </div>
+      </Router>
     );
   }
 }
