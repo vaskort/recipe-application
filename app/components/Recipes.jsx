15794df79cba243b5437f622fcf005e994fed6d3
Vasilis Kortsimelidis <vaskort@gmail.com>
2017-06-14 21:23:14 +0100

    minor css - copy change

--- app/components/Recipes.jsx
+++ app/components/Recipes.jsx
@@ -4,7 +4,6 @@ import { Link } from 'react-router-dom';
 class Recipes extends Component {
 
   getRecipes(){
-    console.log('this props eisai', this.props)
     // if there are recipes then return them
     if (this.props.recipesData.length > 0) {
       return this.props.recipesData.map((recipe, index) =>
@@ -16,7 +15,7 @@ class Recipes extends Component {
             </div>
             <div className="infoContainer">
               <div className="mainIngredients">
-                <h3>Ingredients</h3>
+                <h3>Main ingredients</h3>
                 {recipe.ingredients.map((ingredient, index) => {
                   return <div className="ingredient" key={index}>- {ingredient.name}</div>
                 })}
@@ -42,6 +41,7 @@ class Recipes extends Component {
     let recipes = this.getRecipes();
     return (
         <div className="container">
+          <h1>Recipes</h1>
           <div className="row">
             <ul className="recipeContainer">
               {recipes}
